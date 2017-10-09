bcfb16a546b068f0bbff5531ee70c56a52377440
Marcus Nielsen <marcus.nielsen82@gmail.com>
2017-10-09 22:59:32 +0200

    rm -rf build folder

--- build/server/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = ({ config, server }) => {
-    const onListen = () => {
-        // tslint:disable-next-line:no-console
-        console.log(`Listening on port ${config.MN_SERVER_PORT}`);
-    };
-    server.listen(config.MN_SERVER_PORT, onListen);
-};
